e94e59f593ee1adf50b9884606a9e32b71eea650
shayan heidary <shayanheidary@shayans-MacBook-Pro.local>
2023-06-03 12:14:18 +0200

    first test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Greet from "./components/Greet";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Greet />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greet = () => {
+  return (
+    <div>
+      <h1>Hello this is greeting page</h1>
+      <p>my name is shayan</p>
+    </div>
+  );
+};
+
+export default Greet;
